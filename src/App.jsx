14a9ba0e3cb84767c3717668c569ef1c597ac995
nachoshaman <kalmpurcell@gmail.com>
2024-10-13 07:00:01 -0700

    heroku

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import LandingPage from './Landingpage';
-import SignUp from './signup.jsx';
+import SignUp from './SignUp.jsx';
 import SignIn from './SignIn.jsx';
 import Chatroom from './Chatroom.jsx';
 
