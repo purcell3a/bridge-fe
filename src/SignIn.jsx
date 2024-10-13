b80484024cb614478985e1c3b30a24101754522f
nachoshaman <kalmpurcell@gmail.com>
2024-10-12 22:42:12 -0700

    s

--- src/SignIn.jsx
+++ src/SignIn.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import './Signup.css';
+import './SignIn.css';
 
 const SignUp = () => {
   const navigate = useNavigate();
